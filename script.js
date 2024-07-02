d7e2b53e06ac078512b80dfcaff1c049273a7360
Boitumelo Thobejane <tumijane99@gmail.com>
2024-07-02 12:13:27 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var form = document.getElementById("userForm"),
+var form = document.getElementById("myForm"),
 imgInput = document.querySelector(".imgholder img"),
+file = document.getElementById("imgInput"),
 userName = document.getElementById("name"),
 age = document.getElementById("age"),
 city = document.getElementById("city"),
-file = document.getElementById("imgInput"),
 email = document.getElementById("email"),
 phone = document.getElementById("phone"),
 post = document.getElementById("post"),
@@ -11,7 +11,8 @@ sDate = document.getElementById("sDate"),
 submitBtn = document.querySelector('.submit'),
 userInfo = document.getElementById("data"),
 modal = document.getElementById('userForm'),
-modalTitle = document.querySelector('#userForm .modal-title')
+modalTitle = document.querySelector('#userForm .modal-title'),
+newUserBtn = document.querySelector(".newUser")
 
 
 let getData = localStorage.getItem("userProfile") ? JSON.parse(localStorage.getItem("userProfile")) : []
